d18776aa2fa37b7c98d617732ecc240424bebf07
hasanmiaweb <hasanmiaweb@gmail.com>
2022-04-16 00:32:41 +0600

    user signup and login donegit add .

--- models/User.js
+++ models/User.js
@@ -1,13 +1,38 @@
-const {model, Schema} = require('mongoose');
+const { model, Schema } = require("mongoose");
 
 const userSchema = new Schema({
-    name:String,
-    email: String,
-    password: String,
-    roles:[String],
-    accountStatus: String
-})
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 10,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+      },
+    },
+  },
+  password: {
+    type: String,
+    minlength: 3,
+    required: true,
+  },
+  roles: {
+    type: [String],
+    required: true,
+    default: ["STUDENT"],
+  },
+  accountStatus: {
+    type: String,
+    enum: ["PENDDING", "ACTIVE", "REJECT"],
+    default: "PENDDING",
+  },
+});
 
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
--- server.js
+++ server.js
@@ -1,14 +1,94 @@
-const express = require('express')
-const app = express()
-const port = 3000;
-const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const port = 4000;
+const connectDB = require("./db");
+const User = require("./models/User");
+app.use(express.json())
+const bcrypt = require('bcryptjs');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.post('/register', async (req, res, next) => {
+  const {name, email, password} = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({
+      message:"Something was Wrong"
+    })
+  }
+
+  try {
+    let user = await User.findOne({email:email});
+
+    if (user) {
+      return res.status(400).json({
+        message:"User already Exist"
+      })
+    }
+  
+    user = new User({name, email, password})
+  
+    let salt = await bcrypt.genSalt(10);
+    let hash = await bcrypt.hash(password, salt);
+    user.password = hash
+    await user.save()
+  
+    return res.status(201).json({
+      message:"User Created Successfully", user
+    })
+  } catch (e) {
+    next(e)
+  }
+
+
+});
+
+app.post('/login', async (req, res, next) => {
+  const {email, password} = req.body;
+  try {
+    const user = await User.findOne({email});
+
+    if (!user) {
+      return res.status(400).json({
+        message:"Invalid credentials"
+      })
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password)
+    if (!passwordMatch) {
+      return res.status(400).json({
+        message:"Invalid credentials"
+      })
+    }
+
+    return res.status(200).json({
+      message:"Login Successfully"
+    })
+
+  } catch (e) {
+    next(e)
+  }
+});   
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use((err, req, res, next)=>{
+  console.log(err)
+  res.status(500).json({
+    message:"Server ERROR"
+  })
 })
 
-mongoose.connect('mongodb://127.0.0.1:27017/attendance-system')
 
-app.listen(port, () => {
-  console.log(`I am listening on port ${port}`)
-})
+connectDB("mongodb://127.0.0.1:27017/attendance-db")
+  .then(() => {
+    console.log("Database Connected")
+    app.listen(port, () => {
+      console.log(`I am listening on port ${port}`);
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
